509cf4a55d6e5436be0f332996f3a851f9355923
KD131 <71063280+KD131@users.noreply.github.com>
2023-12-31 02:49:51 +0100

    connection between kafka consumer and producer

--- src/main/java/org/example/weatherservice/listener/Listener.java
+++ src/main/java/org/example/weatherservice/listener/Listener.java
@@ -2,7 +2,10 @@ package org.example.weatherservice.listener;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.example.weatherservice.model.BadWeatherReport;
 import org.example.weatherservice.model.Order;
+import org.example.weatherservice.model.WeatherInformation;
+import org.example.weatherservice.service.KafkaService;
 import org.example.weatherservice.service.WeatherService;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -12,10 +15,12 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class Listener {
     private final WeatherService weatherService;
+    private final KafkaService kafkaService;
 
     @KafkaListener(topics = "NEW_ORDER_PLACED", containerFactory = "orderContainerFactory", groupId = "weather-service")
     public void processNewOrder(Order order) {
         log.info("Received message: {}", order);
+        kafkaService.sendBadWeatherReport(new BadWeatherReport(order.customerId(), new WeatherInformation.Weather(500, "Rain", "light rain", "10n")));
     }
 
 }
--- src/main/java/org/example/weatherservice/service/KafkaService.java
+++ src/main/java/org/example/weatherservice/service/KafkaService.java
@@ -1,12 +1,20 @@
 package org.example.weatherservice.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.weatherservice.enums.Topic;
 import org.example.weatherservice.model.BadWeatherReport;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class KafkaService {
     private final KafkaTemplate<String, BadWeatherReport> kafkaTemplate;
+
+    public void sendBadWeatherReport(BadWeatherReport badWeatherReport) {
+        kafkaTemplate.send(Topic.BAD_WEATHER.getTopicName(), badWeatherReport);
+        log.info("Sent bad weather report: {}", badWeatherReport);
+    }
 }
